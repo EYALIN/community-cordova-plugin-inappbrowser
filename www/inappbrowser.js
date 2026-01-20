603ad1ab792c035f3b667cce512847fc07180940
eyalin <yalin.eliad@gmail.com>
2026-01-20 09:21:12 +0200

    feat: Initial release of community-cordova-plugin-inappbrowser v1.0.0
    - Forked from apache/cordova-plugin-inappbrowser
    - Renamed plugin to community-cordova-plugin-inappbrowser
    - Changed Android package to com.community.cordova.inappbrowser
    - Added full TypeScript type definitions
    - Added InAppBrowserManager class for direct usage without Ionic Native
    - Updated JavaScript API to expose InAppBrowserPlugin global
    - Changed license to MIT
    - Added comprehensive interfaces for TypeScript support
    - Updated README with sponsorship and credits sections

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -1,124 +1,157 @@
 /*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
+ * Licensed under MIT License
  */
 
-(function () {
-    const exec = require('cordova/exec');
-    const channel = require('cordova/channel');
-    const modulemapper = require('cordova/modulemapper');
-    const urlutil = require('cordova/urlutil');
+var PLUGIN_NAME = 'InAppBrowser';
 
-    function InAppBrowser () {
-        this.channels = {
-            beforeload: channel.create('beforeload'),
-            loadstart: channel.create('loadstart'),
-            loadstop: channel.create('loadstop'),
-            loaderror: channel.create('loaderror'),
-            exit: channel.create('exit'),
-            customscheme: channel.create('customscheme'),
-            message: channel.create('message'),
-            download: channel.create('download')
-        };
-    }
+var exec = require('cordova/exec');
+var channel = require('cordova/channel');
+var modulemapper = require('cordova/modulemapper');
+var urlutil = require('cordova/urlutil');
 
-    InAppBrowser.prototype = {
-        _eventHandler: function (event) {
-            if (event && event.type in this.channels) {
-                if (event.type === 'beforeload') {
-                    this.channels[event.type].fire(event, this._loadAfterBeforeload);
-                } else {
-                    this.channels[event.type].fire(event);
-                }
-            }
-        },
-        _loadAfterBeforeload: function (strUrl) {
-            strUrl = urlutil.makeAbsolute(strUrl);
-            exec(null, null, 'InAppBrowser', 'loadAfterBeforeload', [strUrl]);
-        },
-        close: function (eventname) {
-            exec(null, null, 'InAppBrowser', 'close', []);
-        },
-        show: function (eventname) {
-            exec(null, null, 'InAppBrowser', 'show', []);
-        },
-        hide: function (eventname) {
-            exec(null, null, 'InAppBrowser', 'hide', []);
-        },
-        addEventListener: function (eventname, f) {
-            if (eventname in this.channels) {
-                this.channels[eventname].subscribe(f);
-            }
-        },
-        removeEventListener: function (eventname, f) {
-            if (eventname in this.channels) {
-                this.channels[eventname].unsubscribe(f);
-            }
-        },
-
-        executeScript: function (injectDetails, cb) {
-            if (injectDetails.code) {
-                exec(cb, null, 'InAppBrowser', 'injectScriptCode', [injectDetails.code, !!cb]);
-            } else if (injectDetails.file) {
-                exec(cb, null, 'InAppBrowser', 'injectScriptFile', [injectDetails.file, !!cb]);
-            } else {
-                throw new Error('executeScript requires exactly one of code or file to be specified');
-            }
-        },
-
-        insertCSS: function (injectDetails, cb) {
-            if (injectDetails.code) {
-                exec(cb, null, 'InAppBrowser', 'injectStyleCode', [injectDetails.code, !!cb]);
-            } else if (injectDetails.file) {
-                exec(cb, null, 'InAppBrowser', 'injectStyleFile', [injectDetails.file, !!cb]);
-            } else {
-                throw new Error('insertCSS requires exactly one of code or file to be specified');
-            }
-        },
-
-        addDownloadListener: function (success, error) {
-            exec(success, error, 'InAppBrowser', 'downloadListener');
-        }
+/**
+ * InAppBrowser instance constructor
+ * @constructor
+ */
+function InAppBrowserInstance() {
+    this.channels = {
+        beforeload: channel.create('beforeload'),
+        loadstart: channel.create('loadstart'),
+        loadstop: channel.create('loadstop'),
+        loaderror: channel.create('loaderror'),
+        exit: channel.create('exit'),
+        customscheme: channel.create('customscheme'),
+        message: channel.create('message'),
+        download: channel.create('download')
     };
+}
 
-    module.exports = function (strUrl, strWindowName, strWindowFeatures, callbacks) {
+InAppBrowserInstance.prototype = {
+    _eventHandler: function(event) {
+        if (event && event.type in this.channels) {
+            if (event.type === 'beforeload') {
+                this.channels[event.type].fire(event, this._loadAfterBeforeload);
+            } else {
+                this.channels[event.type].fire(event);
+            }
+        }
+    },
+    _loadAfterBeforeload: function(strUrl) {
+        strUrl = urlutil.makeAbsolute(strUrl);
+        exec(null, null, PLUGIN_NAME, 'loadAfterBeforeload', [strUrl]);
+    },
+    /**
+     * Close the InAppBrowser window
+     */
+    close: function() {
+        exec(null, null, PLUGIN_NAME, 'close', []);
+    },
+    /**
+     * Show the InAppBrowser window
+     */
+    show: function() {
+        exec(null, null, PLUGIN_NAME, 'show', []);
+    },
+    /**
+     * Hide the InAppBrowser window
+     */
+    hide: function() {
+        exec(null, null, PLUGIN_NAME, 'hide', []);
+    },
+    /**
+     * Add an event listener
+     * @param {string} eventname - The event name
+     * @param {function} f - The callback function
+     */
+    addEventListener: function(eventname, f) {
+        if (eventname in this.channels) {
+            this.channels[eventname].subscribe(f);
+        }
+    },
+    /**
+     * Remove an event listener
+     * @param {string} eventname - The event name
+     * @param {function} f - The callback function
+     */
+    removeEventListener: function(eventname, f) {
+        if (eventname in this.channels) {
+            this.channels[eventname].unsubscribe(f);
+        }
+    },
+    /**
+     * Execute JavaScript code in the InAppBrowser window
+     * @param {Object} injectDetails - Details with code or file property
+     * @param {function} cb - Callback function
+     */
+    executeScript: function(injectDetails, cb) {
+        if (injectDetails.code) {
+            exec(cb, null, PLUGIN_NAME, 'injectScriptCode', [injectDetails.code, !!cb]);
+        } else if (injectDetails.file) {
+            exec(cb, null, PLUGIN_NAME, 'injectScriptFile', [injectDetails.file, !!cb]);
+        } else {
+            throw new Error('executeScript requires exactly one of code or file to be specified');
+        }
+    },
+    /**
+     * Insert CSS into the InAppBrowser window
+     * @param {Object} injectDetails - Details with code or file property
+     * @param {function} cb - Callback function
+     */
+    insertCSS: function(injectDetails, cb) {
+        if (injectDetails.code) {
+            exec(cb, null, PLUGIN_NAME, 'injectStyleCode', [injectDetails.code, !!cb]);
+        } else if (injectDetails.file) {
+            exec(cb, null, PLUGIN_NAME, 'injectStyleFile', [injectDetails.file, !!cb]);
+        } else {
+            throw new Error('insertCSS requires exactly one of code or file to be specified');
+        }
+    },
+    /**
+     * Add a download listener (Android only)
+     * @param {function} success - Success callback
+     * @param {function} error - Error callback
+     */
+    addDownloadListener: function(success, error) {
+        exec(success, error, PLUGIN_NAME, 'downloadListener');
+    }
+};
+
+/**
+ * InAppBrowser Plugin
+ */
+var InAppBrowserPlugin = {
+    /**
+     * Open a URL in the InAppBrowser
+     * @param {string} strUrl - The URL to load
+     * @param {string} strWindowName - The target window (_self, _blank, _system)
+     * @param {string} strWindowFeatures - Options string
+     * @param {Object} callbacks - Event callbacks object
+     * @returns {InAppBrowserInstance} The InAppBrowser instance
+     */
+    open: function(strUrl, strWindowName, strWindowFeatures, callbacks) {
         // Don't catch calls that write to existing frames (e.g. named iframes).
         if (window.frames && window.frames[strWindowName]) {
-            const origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
+            var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
             return origOpenFunc.apply(window, arguments);
         }
 
         strUrl = urlutil.makeAbsolute(strUrl);
-        const iab = new InAppBrowser();
+        var iab = new InAppBrowserInstance();
 
         callbacks = callbacks || {};
-        for (const callbackName in callbacks) {
+        for (var callbackName in callbacks) {
             iab.addEventListener(callbackName, callbacks[callbackName]);
         }
 
-        const cb = function (eventname) {
+        var cb = function(eventname) {
             iab._eventHandler(eventname);
         };
 
         strWindowFeatures = strWindowFeatures || '';
 
-        exec(cb, cb, 'InAppBrowser', 'open', [strUrl, strWindowName, strWindowFeatures]);
+        exec(cb, cb, PLUGIN_NAME, 'open', [strUrl, strWindowName, strWindowFeatures]);
         return iab;
-    };
-})();
+    }
+};
+
+module.exports = InAppBrowserPlugin;
